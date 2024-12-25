9076fe88786378b8eebb31f322aa5393bf0fc8ed
prasunchakra <lordprasun@gmail.com>
2024-12-25 10:43:13 +0530

    Added Quiz REST API Module

--- quiz_backend/settings.py
+++ quiz_backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'quiz_api',
 ]
 
 MIDDLEWARE = [
@@ -74,9 +76,13 @@ WSGI_APPLICATION = 'quiz_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'quiz',   
+        'USER': 'root',       
+        'PASSWORD': '',   
+        'HOST': 'localhost',           
+        'PORT': '3306',                
     }
 }
 
